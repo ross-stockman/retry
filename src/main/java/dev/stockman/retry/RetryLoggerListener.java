698c1a2601d3bc66d148bd7dd3f037fceadd19dd
ross-stockman <ross.stockman@gmail.com>
2024-12-28 14:02:08 -0600

    improved logging and test coverage

--- src/main/java/dev/stockman/retry/RetryLoggerListener.java
+++ src/main/java/dev/stockman/retry/RetryLoggerListener.java
@@ -19,11 +19,21 @@ class RetryLoggerListener implements RetryListener {
 
     public <T, E extends Throwable> void onError(RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
         boolean nonRetryable = !retryPolicy.canRetry(context) && context.getRetryCount() == 1;
-        log.warn("Attempt: " + context.getRetryCount() + " of " + (nonRetryable ? 1 : retryPolicy.getMaxAttempts()) + ". Exception caught: " + throwable);
+        log.warn("Retry Listener: Attempt: " + context.getRetryCount() + " of " + (nonRetryable ? 1 : retryPolicy.getMaxAttempts()) + ". Exception caught: " + throwable);
         if (nonRetryable) {
-            log.info("Non-retryable exception caught. Will invoke fallback method. Exception caught: " + throwable);
+            log.info("Retry Listener: Non-retryable exception caught. Will invoke fallback method. Exception caught: " + throwable);
         } else if (!retryPolicy.canRetry(context)) {
-            log.info("Retry limit reached. Will invoke fallback method. Exception caught: " + throwable);
+            if (context.getRetryCount() == retryPolicy.getMaxAttempts()) {
+                log.info("Retry Listener: Retry limit reached. Will invoke fallback method. Exception caught: " + throwable);
+            } else {
+                log.info("Retry Listener: Retry chain interrupted by non-retryable exception. Will invoke fallback method. Exception caught: " + throwable);
+            }
+        }
+    }
+
+    public <T, E extends Throwable> void onSuccess(RetryContext context, RetryCallback<T, E> callback, T result) {
+        if (context.getRetryCount() > 0) {
+            log.info("Retry Listener: Attempt: " + (context.getRetryCount() + 1) + " of " + retryPolicy.getMaxAttempts() + ". Success!");
         }
     }
 }
--- src/test/java/dev/stockman/retry/RetryConfigurationTest.java
+++ src/test/java/dev/stockman/retry/RetryConfigurationTest.java
@@ -1,15 +1,23 @@
 package dev.stockman.retry;
 
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mockito;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringJUnitConfig(classes = RetryConfiguration.class)
 @TestPropertySource(properties = {
         "retry.maxAttempts=3",
         "retry.initialInterval=50",
@@ -23,43 +31,185 @@ public class RetryConfigurationTest {
     @Autowired
     private RetryTemplate retryTemplate;
 
-    @ParameterizedTest(name = "untilSuccess={0}, expectedAttempts={1}, expectedOutput={2}")
-    @CsvSource({
-            "10, 3, fallback",
-            "2, 2, success"
-    })
-    void testRetryableExceptions(int untilSuccess, int expectedAttempts, String expectedOutput) {
-        RetryService retryService = new RetryService(untilSuccess);
-        String output = retryTemplate.execute(_ -> {
-            retryService.incrementCounterNullPointerException();
-            return "success";
-        }, c -> "fallback");
-        Assertions.assertThat(output).isEqualTo(expectedOutput);
-        Assertions.assertThat(retryService.getCounter()).isEqualTo(expectedAttempts);
+    private RetryableService retryableService = Mockito.mock(RetryableService.class);
+
+    private TestLogAppender logAppender;
+
+    @BeforeEach
+    void setup() {
+        // Get the logger for the tested class
+        Logger logger = (Logger) LoggerFactory.getLogger(RetryLoggerListener.class);
+
+        // Setup the test log appender
+        logAppender = new TestLogAppender();
+        logAppender.start();
+
+        // Attach the appender to the logger
+        logger.addAppender(logAppender);
     }
 
-    private class RetryService {
-        private final int untilSuccess;
-        private int counter = 0;
+    @Test
+    void testSuccess() {
+        // Arrange
+        Mockito.when(retryableService.test()).thenReturn("success");
 
-        private RetryService(int untilSuccess) {
-            this.untilSuccess = untilSuccess;
+        // Act
+        String output = retryTemplate.execute(_ -> retryableService.test(), c -> "fallback");
+
+        // Assert
+        Assertions.assertThat(output).isEqualTo("success");
+        Mockito.verify(retryableService, Mockito.times(1)).test();
+
+        // validate the retry listener logs
+        Assertions.assertThat(logAppender.getEvents()).isEmpty();
+    }
+
+    @Test
+    void nonRetryableException() {
+        // Arrange
+        Mockito.when(retryableService.test()).thenThrow(new IllegalArgumentException("Non-retryable exception"));
+
+        // Act
+        String output = retryTemplate.execute(_ -> retryableService.test(), c -> "fallback");
+
+        // Assert
+        Assertions.assertThat(output).isEqualTo("fallback");
+        Mockito.verify(retryableService, Mockito.times(1)).test();
+
+        // validate the retry listener logs
+        var logs = logAppender.getEvents();
+        Assertions.assertThat(logs.size()).isEqualTo(2);
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(logs.get(0).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 1 of 1. Exception caught: java.lang.IllegalArgumentException: Non-retryable exception");
+            softly.assertThat(logs.get(1).getFormattedMessage()).isEqualTo("Retry Listener: Non-retryable exception caught. Will invoke fallback method. Exception caught: java.lang.IllegalArgumentException: Non-retryable exception");
+        });
+    }
+
+    @Test
+    void retryableException() {
+        // Arrange
+        Mockito.when(retryableService.test()).thenThrow(new RuntimeException("Retryable exception"));
+
+        // Act
+        String output = retryTemplate.execute(_ -> retryableService.test(), c -> "fallback");
+        Assertions.assertThat(output).isEqualTo("fallback");
+        Mockito.verify(retryableService, Mockito.times(3)).test();
+
+        // validate the retry listener logs
+        var logs = logAppender.getEvents();
+        Assertions.assertThat(logs.size()).isEqualTo(4);
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(logs.get(0).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 1 of 3. Exception caught: java.lang.RuntimeException: Retryable exception");
+            softly.assertThat(logs.get(1).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 2 of 3. Exception caught: java.lang.RuntimeException: Retryable exception");
+            softly.assertThat(logs.get(2).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 3 of 3. Exception caught: java.lang.RuntimeException: Retryable exception");
+            softly.assertThat(logs.get(3).getFormattedMessage()).isEqualTo("Retry Listener: Retry limit reached. Will invoke fallback method. Exception caught: java.lang.RuntimeException: Retryable exception");
+        });
+    }
+
+    @Test
+    void testRetryThenSuccess() {
+        // Arrange
+        Mockito.when(retryableService.test()).thenThrow(new RuntimeException("Retryable exception")).thenReturn("success");
+
+        // Act
+        String output = retryTemplate.execute(_ -> retryableService.test(), c -> "fallback");
+
+        // Assert
+        Assertions.assertThat(output).isEqualTo("success");
+        Mockito.verify(retryableService, Mockito.times(2)).test();
+
+        // validate the retry listener logs
+        var logs = logAppender.getEvents();
+        Assertions.assertThat(logs.size()).isEqualTo(2);
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(logs.get(0).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 1 of 3. Exception caught: java.lang.RuntimeException: Retryable exception");
+            softly.assertThat(logs.get(1).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 2 of 3. Success!");
+        });
+    }
+
+    @Test
+    void testRetryTwiceThenSuccess() {
+        // Arrange
+        Mockito.when(retryableService.test()).thenThrow(new RuntimeException("Retryable exception")).thenThrow(new RuntimeException("Retryable exception")).thenReturn("success");
+
+        // Act
+        String output = retryTemplate.execute(_ -> retryableService.test(), c -> "fallback");
+
+        // Assert
+        Assertions.assertThat(output).isEqualTo("success");
+        Mockito.verify(retryableService, Mockito.times(3)).test();
+
+        // validate the retry listener logs
+        var logs = logAppender.getEvents();
+        Assertions.assertThat(logs.size()).isEqualTo(3);
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(logs.get(0).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 1 of 3. Exception caught: java.lang.RuntimeException: Retryable exception");
+            softly.assertThat(logs.get(1).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 2 of 3. Exception caught: java.lang.RuntimeException: Retryable exception");
+            softly.assertThat(logs.get(2).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 3 of 3. Success!");
+        });
+    }
+
+    @Test
+    void testRetryThenNonRetryable() {
+        // Arrange
+        Mockito.when(retryableService.test()).thenThrow(new RuntimeException("Retryable exception")).thenThrow(new IllegalArgumentException("Non-retryable exception"));
+
+        // Act
+        String output = retryTemplate.execute(_ -> retryableService.test(), c -> "fallback");
+
+        // Assert
+        Assertions.assertThat(output).isEqualTo("fallback");
+        Mockito.verify(retryableService, Mockito.times(2)).test();
+
+        // validate the retry listener logs
+        var logs = logAppender.getEvents();
+        Assertions.assertThat(logs.size()).isEqualTo(3);
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(logs.get(0).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 1 of 3. Exception caught: java.lang.RuntimeException: Retryable exception");
+            softly.assertThat(logs.get(1).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 2 of 3. Exception caught: java.lang.IllegalArgumentException: Non-retryable exception");
+            softly.assertThat(logs.get(2).getFormattedMessage()).isEqualTo("Retry Listener: Retry chain interrupted by non-retryable exception. Will invoke fallback method. Exception caught: java.lang.IllegalArgumentException: Non-retryable exception");
+        });
+    }
+
+    @Test
+    void testRetryTwiceThenNonRetryable() {
+        // Arrange
+        Mockito.when(retryableService.test()).thenThrow(new RuntimeException("Retryable exception")).thenThrow(new RuntimeException("Retryable exception")).thenThrow(new IllegalArgumentException("Non-retryable exception"));
+
+        // Act
+        String output = retryTemplate.execute(_ -> retryableService.test(), c -> "fallback");
+
+        // Assert
+        Assertions.assertThat(output).isEqualTo("fallback");
+        Mockito.verify(retryableService, Mockito.times(3)).test();
+
+        // validate the retry listener logs
+        var logs = logAppender.getEvents();
+        Assertions.assertThat(logs.size()).isEqualTo(4);
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(logs.get(0).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 1 of 3. Exception caught: java.lang.RuntimeException: Retryable exception");
+            softly.assertThat(logs.get(1).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 2 of 3. Exception caught: java.lang.RuntimeException: Retryable exception");
+            softly.assertThat(logs.get(2).getFormattedMessage()).isEqualTo("Retry Listener: Attempt: 3 of 3. Exception caught: java.lang.IllegalArgumentException: Non-retryable exception");
+            softly.assertThat(logs.get(3).getFormattedMessage()).isEqualTo("Retry Listener: Retry limit reached. Will invoke fallback method. Exception caught: java.lang.IllegalArgumentException: Non-retryable exception");
+        });
+    }
+
+    private abstract class RetryableService {
+        public abstract String test();
+    }
+
+    // Custom appender to capture log events
+    private static class TestLogAppender extends AppenderBase<ILoggingEvent> {
+
+        private final List<ILoggingEvent> events = new ArrayList<>();
+
+        @Override
+        protected void append(ILoggingEvent eventObject) {
+            events.add(eventObject);
         }
 
-        public void incrementCounterIllegalArgumentException() {
-            counter++;
-            if (counter < untilSuccess) {
-                throw new IllegalArgumentException("test");
-            }
-        }
-        public void incrementCounterNullPointerException() {
-            counter++;
-            if (counter < untilSuccess) {
-                throw new NullPointerException("test");
-            }
-        }
-        public int getCounter() {
-            return counter;
+        public List<ILoggingEvent> getEvents() {
+            return events;
         }
     }
 }
